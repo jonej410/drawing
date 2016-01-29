3c518dcf8237bd700e32ddf02c2a136bf1ef6c3e
jonej410 <jonej410@newschool.edu>
2016-01-29 15:29:27 -0500

    unfinished

--- sketch.js
+++ sketch.js
@@ -7,5 +7,36 @@ function setup(){
 function draw(){
   background("cadetblue");
 
+  stroke("white");
+  fill("black");
+  ellipse(400,500,600,600);//outer circle
+
+  //right side
+  stroke("white");
+  line(400,500,600,720);
+  line(410,520,675,690);
+
+
+
+  line(400,490,600,720);
+  line(410,490,600,720);
+
+
+
+  stroke("white");
+  line(400,500,680,400);
+
+  //left side
+  stroke("white");
+  line(100,50,400,500);
+
+
+  stroke("black");
+  fill("white");
+  ellipse(400,500,200,200);//inner circle
+
+  fill("black");
+  ellipse(400,500,20,20);//inner inner circle
+
   ellipse(windowWidth/2,windowHeight/2,100,100);
 }
